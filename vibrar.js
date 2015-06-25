ca2c19565936d7ca32c93484a15d841d6a864780
Fili Martínez <fili06604@gmail.com>
2015-06-25 10:46:56 -0500

    Compatibilidad: safari e IE no soportados

--- vibrar.js
+++ vibrar.js
@@ -1,16 +1,20 @@
-document.querySelector("#vibrar").addEventListener("click", function () {
-    navigator.vibrate(1000);
-}, false);
+if (window.navigator && window.navigator.vibrate) {
+   document.querySelector("#vibrar").addEventListener("click", function () {
+       navigator.vibrate(1000);
+   }, false);
 
-document.querySelector("#vibrate-dos").addEventListener("click", function () {
-    navigator.vibrate([200, 100, 200, 100]);
-}, false);
+   document.querySelector("#vibrate-dos").addEventListener("click", function () {
+       navigator.vibrate([200, 100, 200, 100]);
+   }, false);
 
 
-document.querySelector("#vibrar-mucho").addEventListener("click", function () {
-    navigator.vibrate(10000);
-}, false);
+   document.querySelector("#vibrar-mucho").addEventListener("click", function () {
+       navigator.vibrate(10000);
+   }, false);
 
-document.querySelector("#vibrar-off").addEventListener("click", function () {
-     navigator.vibrate(0);
- }, false);
+   document.querySelector("#vibrar-off").addEventListener("click", function () {
+        navigator.vibrate(0);
+    }, false);
+} else {
+   alert("Tu dispositivo no soporta la API de vibración")
+}
